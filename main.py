c7e780c037764f4e995f06bd1bb16b6db061919c
aditya4w <aditya4core@gmail.com>
2025-12-28 12:39:58 +0530

    Initial Commit - Zappy the Expenses Manager

---
+++ main.py
@@ -0,0 +1,48 @@
+import os
+
+class StorageManager:
+    def __init__(self):
+        self.filename = ".zpdata.txt"
+
+        self.filepath = ""
+
+    def setup(self):
+        print("=== ZAPPY Needs Storage Permission For Work >w< ===")
+        print("~ Where Should Zappy Store Your Data? ~")
+        print("1. System (In Home Folder) - Recommended")
+        print("2. Local (This folder)")
+        
+        choice = input("Select (1/2): ")
+
+        if choice == "1":
+            home_dir = os.path.expanduser("~") 
+            folder_path = os.path.join(home_dir, ".zpstorage")
+            
+            if not os.path.exists(folder_path):
+                os.makedirs(folder_path)
+                print(f"Success! at {folder_path}")
+            
+            self.filepath = os.path.join(folder_path, self.filename)
+            
+        else:
+            self.filepath = self.filename
+            print("[Note] Creating file in current folder...")
+
+        if not os.path.exists(self.filepath):
+            with open(self.filepath, "w") as f:
+                f.write("")
+            print("~ New Database Created ~")
+        else:
+            print("~ Found Existing Database ~")
+
+    def get_path(self):
+        return self.filepath
+
+print ("~ HELLO THERE! I\'M ZAPPY.,~")
+
+storage = StorageManager()
+storage.setup()
+
+print(f"Saving Data In {storage.get_path()}")
+print("~ Thank You for the Permissions >w< ~")
+
